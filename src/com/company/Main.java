4368dd2830988a80f85439ef0a73067ae8e8ea01
аслан мекеидзе <mekeidzeaslan9@gmail.com>
2022-02-14 15:13:39 +0300

    mentor1

---
+++ src/com/company/Main.java
@@ -0,0 +1,13 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        int num1 = 6;
+        int num2 = 4;
+        int num3 = num1 + num2;
+
+        System.out.println(" Ето двухзначны  положительное чшсло: " +num3);
+    }
+}
